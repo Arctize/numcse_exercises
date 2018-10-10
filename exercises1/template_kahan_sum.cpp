aa1990467d3ca08a7eedff175deca42aac592efc
arctize <patrick.wicki96@gmail.com>
2018-10-10 08:08:02 +0200

    Ex 1.1 done

--- exercises1/template_kahan_sum.cpp
+++ exercises1/template_kahan_sum.cpp
@@ -1,47 +1,69 @@
-#include<bits/stdc++.h>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-float naive_sum(vector<float> &v) {
-	float sum = 0;
-	for(float x : v){
-		sum += x;
-	}
-	return sum;
+float naive_sum(vector<float>& v)
+{
+    float sum = 0;
+    for (float x : v) {
+	sum += x;
+    }
+    return sum;
 }
 
-float sum(vector<float> v) {
-	float sum = 0;
+// b)
+float sum(vector<float> v)
+{
+    float sum = 0;
 
-	//TODO: Point (b)
+    std::sort(v.begin(), v.end());
+    for (float x : v) {
+	sum += x;
+    }
 
-	return sum;
+    return sum;
 }
 
-float acc_sum(vector<float> &v) {
-	float sum = 0;
+// d)
+float acc_sum(vector<float>& v)
+{
+    float sum = 0;
+    float acc = 0;
+    float prev;
+    for (float x : v) {
+	prev = sum;
+	sum += x;
 
-	//TODO: Point (b)
-
-	return sum;
+	/* Idea:
+		 Add the difference from between how much we tried 
+		 to add, and how much we actually added, to a
+		 seperate variable.
+		 */
+	acc += abs(x - (sum - prev));
+    }
+    return sum + acc;
 }
 
-int main() {
-	srand(time(0));
-	cout << setprecision(15);
-	int N = 1e7;
-	double corr_sum = 0;
-	vector<float> v(N);
-	for (int i = 0; i < N; i++) {
-		double x = 1e-8 + (rand() % 10) * 1e-9;
-		corr_sum += x;
-		v[i] = (float) x;
-	}
-	corr_sum += 1;
-	v[1]=1;
+int main()
+{
+    srand(time(0));
+    cout << setprecision(15);
+    int N = 1e7;
+    double corr_sum = 0;
+    vector<float> v(N);
+    for (int i = 0; i < N; i++) {
+	double x = 1e-8 + (rand() % 10) * 1e-9;
+	corr_sum += x;
+	v[i] = (float)x;
+    }
+    corr_sum += 1;
+    v[1] = 1;
 
-	cout << "naive_sum(v) = " << naive_sum(v) << endl
-		 << "      sum(v) = " << sum(v) << endl 
-		 << "  acc_sum(v) = " << acc_sum(v) << endl
-		 << " correct sum = " << corr_sum << endl;
+    cout << "naive_sum(v) = " << naive_sum(v) << endl
+	 << "      sum(v) = " << sum(v) << endl
+	 << "  acc_sum(v) = " << acc_sum(v) << endl
+	 << " correct sum = " << corr_sum << endl;
 }
