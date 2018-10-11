d58d4b8d79bcd60416f8253c4fefde9ff4e6a026
arctize <patrick.wicki96@gmail.com>
2018-10-11 09:15:05 +0200

    Iterative cutoff

--- exercises1/template_strassen.cpp
+++ exercises1/template_strassen.cpp
@@ -28,9 +28,9 @@ MatrixXf mult_rec(const MatrixXf& A, const MatrixXf& B)
 {
     int N = A.rows();
     MatrixXf C(N, N);
-    if (N < 2) // base case
+    if (N < 16) // fall back to iterative multiplication
     {
-	C(0, 0) = A(0, 0) * B(0, 0);
+	C = mult(A, B);
     } else {
 	int m = N / 2;
 	MatrixXf C11 = mult_rec(A.block(0, 0, m, m), B.block(0, 0, m, m)) + mult_rec(A.block(0, m, m, m), B.block(m, 0, m, m));
@@ -50,7 +50,7 @@ MatrixXf strassen(const MatrixXf& A, const MatrixXf& B)
 {
     int N = A.rows();
     MatrixXf C(N, N);
-    if (N < 10) {
+    if (N < 16) { // fall back to iterative multiplication
 	C = mult(A, B);
     } else {
 	int m = N / 2;
@@ -67,16 +67,16 @@ MatrixXf strassen(const MatrixXf& A, const MatrixXf& B)
 
 	MatrixXf M1 = strassen(A11 + A22, B11 + B22);
 	MatrixXf M2 = strassen(A21 + A22, B11);
-	MatrixXf M3 = strassen(A11	, B12 - B22);
-	MatrixXf M4 = strassen(A22	, B21 - B11);
+	MatrixXf M3 = strassen(A11, B12 - B22);
+	MatrixXf M4 = strassen(A22, B21 - B11);
 	MatrixXf M5 = strassen(A11 + A12, B22);
 	MatrixXf M6 = strassen(A21 - A11, B11 + B12);
 	MatrixXf M7 = strassen(A12 - A22, B21 + B22);
 
-	C.block(0, 0, m, m) = M1 + M4 - M5 + M7;
+	C.block(0, 0, m, m) = M1 + M4 + M7 - M5;
 	C.block(0, m, m, m) = M3 + M5;
 	C.block(m, 0, m, m) = M2 + M4;
-	C.block(m, m, m, m) = M1 - M2 + M3 + M6;
+	C.block(m, m, m, m) = M1 + M3 + M6 - M2;
     }
 
     return C;
@@ -87,7 +87,7 @@ int main()
     srand(time(0));
     cout << setprecision(6) << setfill(' ');
 
-    for (int i = 1; i < 9; i++) {
+    for (int i = 9; i < 10; i++) {
 	int N = 1 << i;
 	cout << "Matrix size = " << N << endl;
 	MatrixXd AA = MatrixXd::Random(N, N);
