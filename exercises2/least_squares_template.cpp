5ce8bddb013316efe643f3563e156bf6ff8c9370
arctize <patrick.wicki96@gmail.com>
2018-10-28 17:30:13 +0100

    Least squares done

--- exercises2/least_squares_template.cpp
+++ exercises2/least_squares_template.cpp
@@ -1,52 +1,60 @@
+#include <eigen3/Eigen/Dense>
 #include <iostream>
-
-#include <Eigen/Dense>
+#include <math.h>
 
 Eigen::MatrixXd Vandermonde(const Eigen::VectorXd &x, int n) {
-    int m = x.size();
-	Eigen::MatrixXd V(m, n);
+  int m = x.size();
+  Eigen::MatrixXd V(m, n);
 
-	// TODO: Task (a)
-	// ...
-	// ...
-	// ...
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < n; j++) {
+      V(i, j) = pow(x[i], j);
+    }
+  }
 
-    return V;
+  return V;
 }
 
 Eigen::VectorXd r(const Eigen::VectorXd &x) {
-	return (1.0 / (1.0 + 25.0 * x.array() * x.array())).matrix();
+  return (1.0 / (1.0 + 25.0 * x.array() * x.array())).matrix();
 }
 
 int main() {
-	int n = 11;				// Number of polynomial coefficients
-	int m;					// Number of samples
-	Eigen::MatrixXd V;		// Vandermonde matrix
-	Eigen::VectorXd x;		// Samples in [-1, 1]
-	Eigen::VectorXd y;		// r(x)
-	Eigen::VectorXd a(n);	// Polynomial coefficients
+  int n = 11;           // Number of polynomial coefficients
+  int m;                // Number of samples
+  Eigen::MatrixXd V;    // Vandermonde matrix
+  Eigen::VectorXd x;    // Samples in [-1, 1]
+  Eigen::VectorXd y;    // r(x)
+  Eigen::VectorXd a(n); // Polynomial coefficients
 
-	Eigen::IOFormat PythonFmt(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", ";\n", "[", "]", "[", "]");
+  Eigen::IOFormat PythonFmt(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ",
+                            ";\n", "[", "]", "[", "]");
 
-	std::cout << "Polynomial coefficients obtained by..." << std::endl;
+  std::cout << "Polynomial coefficients obtained by..." << std::endl;
 
-	// Compute overfitted polynomial coefficients
-	m = n;
-	x.setLinSpaced(m, -1.0, 1.0);
-	// TODO: Task (b)
-	// ...
-	// ...
-	std::cout << "...overfitting:" << std::endl;
-	std::cout << a.transpose().format(PythonFmt) << std::endl;
+  // Compute overfitted polynomial coefficients
+  m = n;
+  x.setLinSpaced(m, -1.0, 1.0);
 
-	// Compute least squares polynomial coefficients
-	m = 3 * n;
-	x.setLinSpaced(m, -1.0, 1.0);
-	// TODO: Task (c)
-	// ...
-	// ...
-	std::cout << "...least squares:" << std::endl;
-	std::cout << a.transpose().format(PythonFmt) << std::endl;
+  // TODO: Task (b)
+  y = r(x);
+  V = Vandermonde(x, n);
+  a = V.partialPivLu().solve(y);
 
-	return 0;
+  std::cout << "...overfitting:" << std::endl;
+  std::cout << a.transpose().format(PythonFmt) << std::endl;
+
+  // Compute least squares polynomial coefficients
+  m = 3 * n;
+  x.setLinSpaced(m, -1.0, 1.0);
+
+  // TODO: Task (c)
+  y = r(x);
+  V = Vandermonde(x, n);
+  a = (V.adjoint() * V).ldlt().solve(V.adjoint()*y);
+
+  std::cout << "...least squares:" << std::endl;
+  std::cout << a.transpose().format(PythonFmt) << std::endl;
+
+  return 0;
 }
