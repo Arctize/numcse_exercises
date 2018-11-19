a647c512c54b094b531cd11f9ad7a4294ae82732
arctize <patrick.wicki96@gmail.com>
2018-11-19 08:08:14 +0100

    Solved ex3

--- exercises3/template_cholesky_qr.cpp
+++ exercises3/template_cholesky_qr.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 
-#include <Eigen/Dense>
-//#include <Eigen/QR>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/QR>
 
 /* @brief QR decomposition from Cholesky decomposition
  * @param[in] A An $m \times n$ matrix
  * @param[out] R The upper triangular matrix from the QR decomposition of $A$
  * @param[out] Q The orthogonal matrix from the QR decomposition of $A$
  */
-void CholeskyQR(const Eigen::MatrixXd &A, Eigen::MatrixXd &R, Eigen::MatrixXd &Q) {
+void CholeskyQR(const Eigen::MatrixXd &A, Eigen::MatrixXd &R,
+                Eigen::MatrixXd &Q) {
 
-	// TODO: Task (c)
-	// ...
-	// ...
-	// ...
-
+  // Task (c)
+  Eigen::MatrixXd AtA = A.transpose() * A;
+  Eigen::LLT<Eigen::MatrixXd> cholesky = AtA.llt();
+  Eigen::MatrixXd L = cholesky.matrixL();
+  R = cholesky.matrixL().transpose();
+  Q = L.triangularView<Eigen::Lower>()
+          .solve(A.transpose())
+          .transpose(); // Why can't we use cholesky.matrixL() instead of L
+                        // here?
 }
 
 /* @brief Direct QR decomposition
@@ -22,36 +27,39 @@ void CholeskyQR(const Eigen::MatrixXd &A, Eigen::MatrixXd &R, Eigen::MatrixXd &Q
  * @param[out] R The upper triangular matrix from the QR decomposition of $A$
  * @param[out] Q The orthogonal matrix from the QR decomposition of $A$
  */
-void DirectQR(const Eigen::MatrixXd &A, Eigen::MatrixXd &R, Eigen::MatrixXd &Q) {
+void DirectQR(const Eigen::MatrixXd &A, Eigen::MatrixXd &R,
+              Eigen::MatrixXd &Q) {
 
-	// TODO: Task (d)
-	// ...
-	// ...
-	// ...
-
+  // Task (d)
+  int m = A.rows(), n = A.cols();
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(A);
+  Eigen::MatrixXd thinQ = (Eigen::MatrixXd::Identity(m, std::min(m, n)));
+  Q = qr.householderQ();
+  thinQ = qr.householderQ() * thinQ;
+  R = qr.matrixQR().triangularView<Eigen::Upper>();
 }
 
 int main() {
-	int m = 3;
-	int n = 2;
-    
-	Eigen::MatrixXd A(m,n);
-	double epsilon = 1.e-8;
-	//A << 3, 5, 1, 9, 7, 1;
-	A << 1, 1, epsilon, 0, 0, epsilon;
-	std::cout << "A =" << std::endl << A << std::endl;
-    
-	Eigen::MatrixXd R, Q;
+  int m = 3;
+  int n = 2;
 
-	CholeskyQR(A, R, Q);
-	std::cout << "CholeskyQR: ===========" << std::endl;
-	std::cout << "R =" << std::endl << R << std::endl;
-	std::cout << "Q =" << std::endl << Q << std::endl;
-    
-	DirectQR(A, R, Q);
-	std::cout << "DirectQR: =============" << std::endl;
-	std::cout << "R =" << std::endl << R << std::endl;
-	std::cout << "Q =" << std::endl << Q << std::endl;
+  Eigen::MatrixXd A(m, n);
+  double epsilon = 1.e-8;
+  // A << 3, 5, 1, 9, 7, 1;
+  A << 1, 1, epsilon, 0, 0, epsilon;
+  std::cout << "A =" << std::endl << A << std::endl;
 
-	return 0;
+  Eigen::MatrixXd R, Q;
+
+  CholeskyQR(A, R, Q);
+  std::cout << "CholeskyQR: ===========" << std::endl;
+  std::cout << "R =" << std::endl << R << std::endl;
+  std::cout << "Q =" << std::endl << Q << std::endl;
+
+  DirectQR(A, R, Q);
+  std::cout << "DirectQR: =============" << std::endl;
+  std::cout << "R =" << std::endl << R << std::endl;
+  std::cout << "Q =" << std::endl << Q << std::endl;
+
+  return 0;
 }
--- exercises3/template_eigenfaces.cpp
+++ exercises3/template_eigenfaces.cpp
@@ -1,74 +1,102 @@
-#include <iostream> 
+#include <eigen3/Eigen/Dense>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <eigen3/Eigen/Dense>
 
 using namespace Eigen;
 
 VectorXd load_pgm(const std::string &filename) {
-	// returns a picture as a flattened vector
+  // returns a picture as a flattened vector
 
-	int row = 0, col = 0, rows = 0, cols = 0;
+  int row = 0, col = 0, rows = 0, cols = 0;
 
-	std::ifstream infile(filename);
-	std::stringstream ss;
-	std::string inputLine = "";
+  std::ifstream infile(filename);
+  std::stringstream ss;
+  std::string inputLine = "";
 
-	// First line : version
-	std::getline(infile,inputLine);
+  // First line : version
+  std::getline(infile, inputLine);
 
-	// Second line : comment
-	std::getline(infile,inputLine);
+  // Second line : comment
+  std::getline(infile, inputLine);
 
-	// Continue with a stringstream
-	ss << infile.rdbuf();
-	// Third line : size
-	ss >> cols >> rows;
+  // Continue with a stringstream
+  ss << infile.rdbuf();
+  // Third line : size
+  ss >> cols >> rows;
 
-	VectorXd picture(rows*cols);
+  VectorXd picture(rows * cols);
 
-	// Following lines : data
-	for(row = 0; row < rows; ++row) {
-		for (col = 0; col < cols; ++col) {
-			int val;
-			ss >> val;
-			picture(col*rows + row) = val;
-		}
-	}
+  // Following lines : data
+  for (row = 0; row < rows; ++row) {
+    for (col = 0; col < cols; ++col) {
+      int val;
+      ss >> val;
+      picture(col * rows + row) = val;
+    }
+  }
 
-	infile.close();
-	return picture;
+  infile.close();
+  return picture;
 }
 
+using namespace std;
+using namespace Eigen;
+
 int main() {
-	
-	int h = 231;
-	int w = 195;
-	int M = 15;
 
-	MatrixXd faces(h*w, M);
-	VectorXd meanFace(h*w);
-	
-	// loads pictures as flattened vectors into faces
-	for (int i=0; i<M; i++) {
-		std::string filename = "./basePictures/subject"+ 
-			std::to_string(i+1) + ".pgm";
-		VectorXd flatPic = load_pgm(filename);
-		faces.col(i) = flatPic;
-		
-		// TODO: Point (b)
-	}
+  int h = 231;
+  int w = 195;
+  int M = 15;
 
-	
-	// TODO: Point (e)
+  MatrixXd faces(h * w, M);
+  VectorXd meanFace(h * w);
 
-	// try to recognize a test face
-	string testPicName = "./testPictures/subject01.happy.pgm";
-	VectorXd newFace = load_pgm(testPicName);
+  // loads pictures as flattened vectors into faces
+  for (int i = 0; i < M; i++) {
+    string filename = "./basePictures/subject" + to_string(i + 1) + ".pgm";
+    VectorXd flatPic = load_pgm(filename);
+    faces.col(i) = flatPic;
 
-	// TODO: Point (f)
-	
-	// TODO: Point (g)
+    // TODO: Point (b)
+    meanFace += flatPic;
+  }
 
+  meanFace /= M;
+  faces.colwise() -= meanFace;
+
+  JacobiSVD<MatrixXd> svd(faces, ComputeThinU);
+  MatrixXd u = svd.matrixU();
+
+  //MatrixXd projectedFaces(1000, M);
+  //for (int i = 0; i < faces.cols(); i++) {
+  //  projectedFaces.col(i) = u.transpose() * (faces.col(i) - meanFace);
+  //}
+  MatrixXd projectedFaces = u.transpose() * faces;
+
+  string testPicture = "./testPictures/Narutowicz.pgm";
+  VectorXd newFace = load_pgm(testPicture);
+  VectorXd d = u.transpose() * (newFace - meanFace);
+
+  int minIndex = 0;
+  int minNorm = (projectedFaces.col(0) - d).norm();
+  for (int i = 1; i < faces.cols(); i++) {
+    double norm = (projectedFaces.col(i) - d).norm();
+    if (norm < minNorm) {
+      minNorm = norm;
+      minIndex = i;
+    }
+  }
+
+  cout << minIndex << endl;
+
+  // TODO: Point (g)
+  //  MatrixXd projFaces = u.transpose() * faces;
+  // VectorXd projNewFace = u.transpose() * (newFace - meanFace);
+  // int indexMinNorm;
+  //(projFaces.colwise() -
+  // projNewFace).colwise().norm().minCoeff(&indexMinNorm); cout << testPicName
+  // << " is identified as subject " << indexMinNorm + 1
+  //     << endl;
 }
